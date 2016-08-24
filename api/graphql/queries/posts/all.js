35a3384e4fc3fd021ba95742e9604458170e35d3
Jahmai OSullivan <jahmai.osullivan@gmail.com>
2016-08-24 16:24:09 -0400

    refactor since we renamed column. No alias required

--- api/graphql/queries/posts/all.js
+++ api/graphql/queries/posts/all.js
@@ -14,14 +14,13 @@ export default  {
   },
   async resolve({user}, { page }) {
     console.log(`finding posts now for user ${util.inspect(user)}`);
-    const commentsAlias = {model: Comment, as: 'comments' }; // where: { name: { $like: '%ooth%' }};
     return await Post.findAll({
       offset:page * config.paging.rows - config.paging.rows,
       limit :config.paging.rows,
-      include: [commentsAlias],
+      include: [Comment],
       order: [
         [ 'createdAt', 'DESC' ],
-        [commentsAlias, 'createdAt', 'ASC']
+        [Comment, 'createdAt', 'ASC']
       ]
     });
   }
