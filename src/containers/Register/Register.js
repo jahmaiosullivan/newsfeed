db55e21afb99718ff345c4ee1c3848b35f6be72c
Jahmai OSullivan <jahmai.osullivan@gmail.com>
2016-08-25 17:13:16 -0400

    add validation to registration page

--- src/containers/Register/Register.js
+++ src/containers/Register/Register.js
@@ -2,25 +2,35 @@ import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
 import Helmet from 'react-helmet';
 import * as authActions from 'redux/reducers/authReducer';
+import { createForm } from 'rc-form';
 
 @connect(
   () => ({}),
   authActions)
+@createForm()
 export default class Register extends Component {
   static propTypes = {
+    form: PropTypes.object,
     register: PropTypes.func
   };
 
   handleSubmit = (event) => {
     event.preventDefault();
-    const email = this.refs.email;
-    const password = this.refs.password;
-    this.props.register(email.value, password.value);
-    email.value = '';
-    password.value = '';
+    const {register, form} = this.props;
+
+    form.validateFields((error, values) => {
+      console.log(error, values);
+
+      if (!error) {
+        const { email, password } = values;
+        register( email.trim(), password.trim() );
+        form.setFieldsValue({email: '', password: ''});
+      }
+    });
   };
 
   render() {
+    const { form: {getFieldProps, getFieldError} } = this.props;
     return (
       <div>
         <Helmet title="Register"/>
@@ -28,8 +38,16 @@ export default class Register extends Component {
         <div>
           <form className="login-form form-inline" onSubmit={this.handleSubmit}>
             <div className="form-group">
-              <input type="text" ref="email" placeholder="Enter your email" className="form-control"/>
-              <input type="password" ref="password" placeholder="Enter password" className="form-control"/>
+              <input {...getFieldProps('email', { initialValue: '', rules: [{required: true, min: 3, whitespace: true}] })}
+                type="text"
+                placeholder="Enter your email"
+              />
+              {getFieldError('password') && getFieldError('password').join(',')}
+              <input {...getFieldProps('password', { initialValue: '', rules: [{required: true, min: 3, whitespace: true}] })}
+                type="password"
+                placeholder="Enter your password"
+              />
+              {getFieldError('email') && getFieldError('email').join(',')}
             </div>
             <button className="btn btn-success" onClick={this.handleSubmit}><i className="fa fa-sign-in"/>{' '}Register
             </button>
