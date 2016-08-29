a38239b5ff29946764820284fefb4b4c8c5ea194
Jahmai OSullivan <jaosulli@microsoft.com>
2016-08-29 16:05:17 -0400

    Move styles into render Use default theme for image overlay

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -5,71 +5,10 @@ import lodash from 'lodash';
 import TimeAgoDate from '../TimeAgoDate/TimeAgoDate';
 import Gallery from '../Gallery/Gallery';
 
-const styles = require( './Post.scss' );
-
-const PostMainImage = ({images}) => {
-  return (<div className={styles.imageTile}>
-    {images && <img src={images[0].preview} alt=""/>}
-  </div>);
-};
-
-const TagList = ({tags}) => {
+const TagList = ({tags, styles}) => {
   return (<div>{tags && tags.map( (tag) => {
     return (<a href="#" className={styles.hashtag}>{`#${tag}`}</a>);
   } )}</div>);
-};
-const theme = {
-  // container
-  container: {background: 'rgba(255, 255, 255, 0.9)'},
-
-  // arrows
-  arrow: {
-    backgroundColor: 'rgba(255, 255, 255, 0.8)',
-    fill: '#222',
-    opacity: 0.6,
-    transition: 'opacity 200ms',
-
-    ':hover': {
-      opacity: 1
-    }
-  },
-  arrow__size__medium: {
-    borderRadius: 40,
-    height: 40,
-    marginTop: -20,
-
-    '@media (min-width: 768px)': {
-      height: 70,
-      padding: 15
-    }
-  },
-  arrow__direction__left: {marginLeft: 10},
-  arrow__direction__right: {marginRight: 10},
-
-  // header
-  close: {
-    fill: '#D40000',
-    opacity: 0.6,
-    transition: 'all 200ms',
-
-    ':hover': {
-      opacity: 1
-    }
-  },
-
-  // footer
-  footer: {
-    color: 'black'
-  },
-  footerCount: {
-    color: 'rgba(0, 0, 0, 0.6)'
-  },
-
-  // thumbnails
-  thumbnail: {},
-  thumbnail__active: {
-    boxShadow: '0 0 0 2px #00D8FF'
-  }
 };
 
 export default class Post extends Component {
@@ -103,6 +42,7 @@ export default class Post extends Component {
   }
 
   render() {
+    const styles = require( './Post.scss' );
     const {users, currentUser, saveFile, body, editing, id, title, createdAt, createdBy, editPost, editPostStop, deletePost, editPostStart, images} = this.props;
     const postCreator = lodash.find( users, (postUser) => {
       return postUser.id === createdBy;
@@ -135,12 +75,12 @@ export default class Post extends Component {
           <div className="clearfix"></div>
           <div className={styles.imageTile}>
             {images && images !== null && <Gallery images={images.map((img) => ({
-            src: img.preview,
-            thumbnail: img.preview,
-            caption: '',
-            orientation: 'square',
-            srcset: []
-          }))} theme={theme} showThumbnails />}
+              src: img.preview,
+              thumbnail: img.preview,
+              caption: '',
+              orientation: 'square',
+              srcset: []
+            }))} showThumbnails />}
           </div>
           <div>
             <div className="row">
@@ -155,7 +95,7 @@ export default class Post extends Component {
             <div className={ styles.postDetails + ' row'}>
               <h3>{title}</h3>
               <p>{body}</p>
-              <TagList tags={tags}/>
+              <TagList tags={tags} styles={styles} />
             </div>
             <div>
               <CommentList {...this.props} onChanged={() => this.handleCommentListChanged()}/>
