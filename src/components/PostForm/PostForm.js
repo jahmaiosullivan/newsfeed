6a408b753d4890b49c90eb612b0b35b9b93e7b29
Jahmai OSullivan <jaosulli@microsoft.com>
2016-08-30 13:55:21 -0400

    Preliminary tags input

--- src/components/PostForm/PostForm.js
+++ src/components/PostForm/PostForm.js
@@ -1,4 +1,5 @@
 import React, {Component, PropTypes} from 'react';
+import { WithContext as ReactTags } from 'react-tag-input';
 import DropZone from '../../components/ImageUpload/DropZone';
 import ValidatedTextInput from './ValidatedTextInput';
 import ValidatedTextArea from './ValidatedTextArea';
@@ -19,7 +20,10 @@ export default class PostForm extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { images: this.props.images ? this.props.images : [] };
+    this._handleAddition = (tag) => { this.handleAddition(tag);};
+    this._handleDelete = (index) => { this.handleDelete(index);};
+    this._handleDrag = (tag, currPos, newPos) => { this.handleDrag(tag, currPos, newPos); };
+    this.state = { images: this.props.images ? this.props.images : [], tags: [ {id: 1, text: 'Apples'} ], suggestions: ['Banana', 'Mango', 'Pear', 'Apricot'] };
   }
 
   handleImagesChange(images) {
@@ -43,6 +47,32 @@ export default class PostForm extends Component {
     });
   }
 
+  handleDelete(index) {
+    const tags = this.state.tags;
+    tags.splice(index, 1);
+    this.setState({tags: tags});
+  }
+
+  handleAddition(tag) {
+    const tags = this.state.tags;
+    tags.push({
+      id: tags.length + 1,
+      text: tag
+    });
+    this.setState({tags: tags});
+  }
+
+  handleDrag(tag, currPos, newPos) {
+    const tags = this.state.tags;
+
+    // mutate array
+    tags.splice(currPos, 1);
+    tags.splice(newPos, 0, tag);
+
+    // re-render
+    this.setState({ tags: tags });
+  }
+
   clearForm() {
     this.props.form.setFieldsValue({details: '', location: ''});
     this.setState({images: []});
@@ -50,7 +80,9 @@ export default class PostForm extends Component {
 
   render() {
     const { id, title, body, uploadFileHandler, form: {getFieldError, getFieldProps}} = this.props;
+    const { tags, suggestions } = this.state;
     const styles = require('./PostForm.scss');
+    const tagStyles = require('./Tags.scss');
     const basicRules = [{required: true, min: 3, whitespace: true}];
     const locationErrors = getFieldError('location') ? getFieldError('location') : [];
     const detailErrors = getFieldError('details') ? getFieldError('details') : [];
@@ -60,6 +92,19 @@ export default class PostForm extends Component {
         <div>
           <ValidatedTextInput getFieldProps={getFieldProps} name="location" rules={basicRules} placeHolderText="Location ..." value={title} />
           <ValidatedTextArea className={styles.details} getFieldProps={getFieldProps} name="details" rules={basicRules} placeHolderText="Details ..." value={body} rows={4} maxLength={300} />
+          <ReactTags tags={tags}
+                     classNames={{
+                       tags: tagStyles.tags,
+                       tagInput: tagStyles.tagInput,
+                       selected: tagStyles.selected,
+                       tag: tagStyles.tag,
+                       remove: tagStyles.remove,
+                       suggestions: tagStyles.suggestions
+                     }}
+                     suggestions={suggestions}
+                     handleDelete={this._handleDelete}
+                     handleAddition={this._handleAddition}
+                     handleDrag={this._handleDrag} />
         </div>
         <div className={styles.imagesContainer}>
           <DropZone ref="dropzone" addPhotoStyle={styles.addPhoto} images={this.state.images} uploadImageHandler={uploadFileHandler} onChangeHandler={(event) => { this.handleImagesChange(event); }} />
